28bef4e837cb0edee0327f9e3173a0839bc182ab
hc018 <hc018.ai@gmail.com>
2025-11-13 10:31:01 +0800

    feat : 改為使用QueueKeyMapper，對應Queue名稱

---
+++ Queue/QueueKeyMapper.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsumerConsoleApp.Queue
+{
+    public static class QueueKeyMapper
+    {
+        public static string ToQueueName(this QueueKey key) => key switch
+        {
+            QueueKey.AllPhotoOriginalAnalysis => "photo.analysis.all.command",
+
+            _ => key.ToString().ToLower()
+        };
+    }
+}
